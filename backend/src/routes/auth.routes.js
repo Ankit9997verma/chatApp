39f64233cb98ea8fb9f75dcfa4fb23794ec09a0f
Ankit9997verma <ankit9997verma@gmail.com>
2026-04-06 12:25:55 +0530

    LOGIN LOGOUT

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,15 +1,10 @@
 import express from "express";
-import {signup} from "../controllers/auth.controller.js";
+import {signup, login , logout} from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login" , (req , res)=>{
-    const { email, password } = req.body || {};
-    if (!email || !password) return res.status(400).json({ message: "Email and password are required" });
-    res.send("login endpoint");
-})
-router.post("/logout" , (req , res)=>{
-    res.send("logout endpoint");
-})
+router.post("/login" , login);
+router.post("/logout" , logout);
 export default router;
+
 
